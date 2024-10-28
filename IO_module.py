f25f0663c4f615cbb2af8d629031d36532e8cf79
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2024-10-28 18:53:56 +0900

    結果を出力する関数を追加

--- IO_module.py
+++ IO_module.py
@@ -3,10 +3,12 @@
 import os
 import sys
 
-
 def file_check(file_path:str) -> None:
     if os.path.exists(file_path):
-        pass
+        print(file_path + "：存在を確認しました。")
     else:
         print("必要なファイルが存在しません。ファイルを確認してください。\n\n 足りないファイル：" + file_path )
         sys.exit()
+
+def print_result() -> None:
+    
