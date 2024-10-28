99d502858ebfd51282ed7b75be99d5e5dba72157
OrenoYomenoOtto <nakamurahirotada160@gmail.com>
2024-10-28 18:49:06 +0900

    ファイル確認関数の追加

---
+++ IO_module.py
@@ -0,0 +1,12 @@
+#coding utf8
+
+import os
+import sys
+
+
+def file_check(file_path:str) -> None:
+    if os.path.exists(file_path):
+        pass
+    else:
+        print("必要なファイルが存在しません。ファイルを確認してください。\n\n 足りないファイル：" + file_path )
+        sys.exit()
